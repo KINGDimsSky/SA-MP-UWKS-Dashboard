f2d93496899050136ade89b6b3979f5a15e43051
KINGDimsSky <dimasmarcelo2004@gmail.com>
2024-10-24 13:07:21 +0700

    Fixing Sidebar Nav

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,10 @@
 import { Flag, House, Settings, User } from 'lucide-react'
 import { sidebarItem } from '../config'
+import { useState } from 'react'
 
 const Dashboard = () => {
+
+  const [activeIndex, setActiveIndex] = useState(0);
 
   const iconMap = {
     House: House,
@@ -15,9 +18,9 @@ const Dashboard = () => {
         <div className="flex border-r border-gray-300 items-center  justify-between flex-col w-72 h-screen">
             <h2 className='text-3xl ml-2 mt-4 pt-3 font-semibold'>UWKS RolePlay</h2>
               <ul className='flex flex-col w-full gap-y-4'>
-                {sidebarItem.map((e) => {
+                {sidebarItem.map((e, index) => {
                 const MyIcons = iconMap[e.icon]
-                return <li key={e.title} className='md:pl-12 md:text-base text-sm items-center gap-5 flex hover:text-pink-500 hover:bg-pink-50 p-2 font-semibold tracking-tight'><MyIcons size={23}/> {e.title}</li>
+                return <li key={e.title} onClick={() => setActiveIndex(index)} className={`md:pl-12 md:text-base text-sm items-center gap-5 flex ${activeIndex === index ? `bg-pink-50 text-pink-500` : ``} hover:text-pink-500 hover:bg-pink-50 p-2 font-semibold tracking-tight`}><MyIcons size={23}/> {e.title}</li>
                 })}
               </ul>
             <div className="text-xs text-gray-500 p-2 w-full flex flex-col items-center">
