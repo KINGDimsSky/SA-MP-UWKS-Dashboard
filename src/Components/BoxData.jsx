b4bc2f745db9c9abc886c716707e358844f6ed53
KINGDimsSky <dimasmarcelo2004@gmail.com>
2024-10-31 19:39:38 +0700

    Fixing Responsive On Dashboard

--- src/Components/BoxData.jsx
+++ src/Components/BoxData.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const BoxData = ({background, icon: Users}) => {
+const BoxData = ({background, icon: Users, title}) => {
   return (
-    <div className={`box flex flex-col ${background} p-4  w-64 flex-wrap h-36 rounded-lg p-4"`}>
+    <div className={`box flex flex-col ${background} p-4 w-40 md:w-80 flex-wrap h-36 rounded-lg p-4"`}>
         {Users && <Users size ={24} className="mb-2"/>}
-        <h2>Player Info</h2>
+        <h2 className='md:text-base text-sm'>{title}</h2>
     </div>
   )
 }
--- src/Components/HomeDash.jsx
+++ src/Components/HomeDash.jsx
@@ -5,11 +5,20 @@ import { Users, Activity, Gamepad } from 'lucide-react';
 
 const HomeDash = () => {
   return (
-    <div className="flex gap-6">
-       <BoxData background="bg-yellow-200" icon={Users}/>
-       <BoxData background="bg-pink-200" icon={Activity}/>
-       <BoxData background="bg-green-200" icon={Gamepad}/>
+    <div className="flex flex-col md:items-center">
+       <div className="flex flex-wrap gap-6 mb-12">
+         <BoxData background="bg-yellow-200" icon={Users} title="Player Info"/>
+         <BoxData background="bg-pink-200" icon={Activity} title="Server Status"/>
+         <BoxData background="bg-green-200" icon={Gamepad} title="Gamemode"/>
+         <div className="bg-blue-200 w-[26em] p-4">
+            <h2 className='text-white font-semibold text-xl'>Server Running Fluenlty</h2>
+         </div>
+       </div>
+       <div className="data bg-gray-100 w-full md:w-[75rem]">
+        <h2 className=''>Data</h2>
+       </div>
     </div>
+   
   )
 }
 
