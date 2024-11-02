fdd75834ffa25eb99fb3e6af32e1064f4c516a17
KINGDimsSky <dimasmarcelo2004@gmail.com>
2024-11-02 17:59:05 +0700

    Adding Dashboard Content

--- src/Components/BoxData.jsx
+++ src/Components/BoxData.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
-const BoxData = ({background, icon: Users, title}) => {
+const BoxData = ({background, icon: Users, title, data}) => {
   return (
     <div className={`box flex flex-col ${background} p-4 w-40 md:w-80 flex-wrap h-36 rounded-lg p-4"`}>
         {Users && <Users size ={24} className="mb-2"/>}
-        <h2 className='md:text-base text-sm'>{title}</h2>
+        <h2 className='md:text-base text-sm mb-2'>{title}</h2>
+        <h2 className='font-bold text-xl'>{data}</h2>
     </div>
   )
 }
